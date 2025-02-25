c3275f3a26b62490e1b4da59c8c5d3e60382698e
Livia Canuto <liviacanuto01@gmail.com>
2025-02-25 17:28:31 -0300

    first commit

---
+++ src/main/java/com/example/hangOut_api/HangOutApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hangOut_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangOutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangOutApiApplication.class, args);
+	}
+
+}
